e46129352661770de6fccf8e06ca0398603b2692
ciro.azevedo <ciro.azevedo@somosphi.com>
2021-08-01 19:55:01 -0300

    add test unit in controller

--- src/presentation/http/controllers/v1/users/find-by-di/find-by-id_test.go
+++ src/presentation/http/controllers/v1/users/find-by-di/find-by-id_test.go
@@ -1,8 +1,9 @@
 package v1_user
 
 import (
+	"errors"
 	"go-api/src/core/entities/user"
-	user_use_case "go-api/src/core/useCases/user"
+	entity_user "go-api/src/core/entities/user"
 	"go-api/src/main/container"
 	ports_http "go-api/src/presentation/http/ports"
 	"testing"
@@ -11,11 +12,11 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type UserServiceMock struct {
+type MockUserCase struct {
 	mock.Mock
 }
 
-func (mock *UserServiceMock) GetById(id string) (*user.User, error) {
+func (mock *MockUserCase) GetUser(id string) (*user.User, error) {
 	arg := mock.Called()
 	result := arg.Get(0)
 	return result.(*user.User), arg.Error(1)
@@ -25,20 +26,18 @@ func newMockUser() *user.User {
 	user, _ := user.NewUser("test", "test", "test")
 	return user
 }
-func Test_GetUser(t *testing.T) {
+func Test_Http_Controller_GetUser(t *testing.T) {
 	t.Run("succeffully", func(t *testing.T) {
 		userMock := newMockUser()
-		mockRepo := new(UserServiceMock)
+		mockRepo := new(MockUserCase)
+		id := "752ea551-5e6a-4382-859c-cd09fbe50110"
 
 		mockRepo.On("GetUser").Return(userMock, nil)
 
-		container := &container.Container{
-			UserService: user_use_case.Service{},
-		}
+		testService := NewController(&container.Container{
+			UserService: mockRepo,
+		})
 
-		testService := NewController(container)
-
-		id := "752ea551-5e6a-4382-859c-cd09fbe50110"
 		result, err := testService.LoadRoute().Handle(ports_http.HttpRequest{
 			Params: ports_http.Params{
 				ports_http.Param{Key: "id", Value: id},
@@ -47,14 +46,63 @@ func Test_GetUser(t *testing.T) {
 
 		assert.Nil(t, err)
 		assert.NotNil(t, result)
-
+		assert.Equal(t, &ports_http.HttpResponse{
+			Data:   userMock,
+			Status: 200,
+		}, result)
 	})
 
-	// 	t.Run("error internal", func(t *testing.T) {
+	t.Run("error internal", func(t *testing.T) {
+		userMock := newMockUser()
+		mockRepo := new(MockUserCase)
+		id := "752ea551-5e6a-4382-859c-cd09fbe50110"
 
-	// 	})
+		mockRepo.On("GetUser").Return(userMock, errors.New("test"))
 
-	// 	t.Run("error user not found", func(t *testing.T) {
+		testService := NewController(&container.Container{
+			UserService: mockRepo,
+		})
 
-	// 	})
+		result, err := testService.LoadRoute().Handle(ports_http.HttpRequest{
+			Params: ports_http.Params{
+				ports_http.Param{Key: "id", Value: id},
+			},
+		})
+
+		assert.NotNil(t, err)
+		assert.Equal(t, err, &ports_http.HttpResponseError{
+			Data: ports_http.HttpError{
+				Code:    "INTERNAL_ERROR",
+				Message: "internal error",
+			},
+			Status: 500,
+		}, result)
+	})
+
+	t.Run("error user not found", func(t *testing.T) {
+		userMock := newMockUser()
+		mockRepo := new(MockUserCase)
+		id := "752ea551-5e6a-4382-859c-cd09fbe50110"
+
+		mockRepo.On("GetUser").Return(userMock, entity_user.ErrUserNotFound)
+
+		testService := NewController(&container.Container{
+			UserService: mockRepo,
+		})
+
+		result, err := testService.LoadRoute().Handle(ports_http.HttpRequest{
+			Params: ports_http.Params{
+				ports_http.Param{Key: "id", Value: id},
+			},
+		})
+
+		assert.NotNil(t, err)
+		assert.Equal(t, err, &ports_http.HttpResponseError{
+			Data: ports_http.HttpError{
+				Code:    "USER_NOT_FOUND",
+				Message: entity_user.ErrUserNotFound.Error(),
+			},
+			Status: 400,
+		}, result)
+	})
 }
