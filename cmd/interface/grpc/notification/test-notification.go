4ceb47dc423c3e759a0c7aa71b65e415923702a2
ciro.azevedo <ciro.azevedo@4all.com>
2022-08-16 08:06:49 -0300

    fix test connection grpc

--- cmd/interface/grpc/notification/test-notification.go
+++ cmd/interface/grpc/notification/test-notification.go
@@ -1,27 +1,27 @@
-package userpb
+package notificaitonpb
 
 import (
 	"context"
-	"go-api/cmd/interface/grpc/user/pb"
+	"go-api/cmd/interface/grpc/notification/pb"
 	"go-api/cmd/main/container"
 	"log"
 )
 
-type userPB struct {
+type notificationPB struct {
 	container *container.Container
 }
 
-func New(c *container.Container) *userPB {
-	return &userPB{container: c}
+func New(c *container.Container) *notificationPB {
+	return &notificationPB{container: c}
 }
 
-func (npb *userPB) Verify(ctx context.Context, req *pb.ResquestUser) (*pb.ResponseUser, error) {
+func (npb *notificationPB) Verify(ctx context.Context, req *pb.ResquestNotification) (*pb.ResponseNotificaiton, error) {
 
 	log.Default().Println("----> request by grpc: Name:" + req.List.Name + " Describe: " + req.List.Describe)
 
 	npb.container.ListUsersUseCase.ListUsers()
 
-	return &pb.ResponseUser{
+	return &pb.ResponseNotificaiton{
 		Event: &pb.List{
 			Name:     "succeffully",
 			Describe: "grpc connection succeffully",
--- cmd/main/modules/grpc/grpc-config.go
+++ cmd/main/modules/grpc/grpc-config.go
@@ -1,13 +1,13 @@
 package grpc
 
 import (
-	notificationpb "go-api/cmd/interface/grpc/user"
-	"go-api/cmd/interface/grpc/user/pb"
+	notificaitonpb "go-api/cmd/interface/grpc/notification"
+	"go-api/cmd/interface/grpc/notification/pb"
 	"go-api/cmd/main/container"
 	"log"
 )
 
 func (s *GRPCServer) LoadServices(c *container.Container) {
-	pb.RegisterNotificationInterfacePbServer(s.Engine, notificationpb.New(c))
+	pb.RegisterTestNotificationInterfacePbServer(s.Engine, notificaitonpb.New(c))
 	log.Default().Print("gRPC: Services registered")
 }
