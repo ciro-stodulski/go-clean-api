33046874c1b45c38d4e02d92313423ff26b4017c
ciro-stodulski <ciro.sda@gmail>
2022-09-11 19:40:05 -0300

    TEST

--- scripts/kubernetes/builder-deployments.sh
+++ scripts/kubernetes/builder-deployments.sh
@@ -1,24 +1,4 @@
-#!/bin/bash
-function readJson {  
-  UNAMESTR=`uname`
-  if [[ "$UNAMESTR" == 'Linux' ]]; then
-    SED_EXTENDED='-r'
-  elif [[ "$UNAMESTR" == 'Darwin' ]]; then
-    SED_EXTENDED='-E'
-  fi; 
 
-  VALUE=`grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//'`
-
-  if [ ! "$VALUE" ]; then
-    echo "Error: Cannot find \"${2}\" in ${1}" >&2;
-    exit 1;
-  else
-    echo $VALUE ;
-  fi; 
-}
-
-APP_VERSION=`readJson ../../package.json version` || exit 1;
-NAMESPACE=`readJson ../../package.json namespace` || exit 1;
 
 sed -i  "s/\$PROJECT_NAME/${APP_NAME}/"   deployment.yaml
 sed -i  "s/\$APP_VERSION/${APP_VERSION}/"     deployment.yaml
